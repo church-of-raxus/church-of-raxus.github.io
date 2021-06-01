44626a314e622135572f3f0f9a016b33db9d0a68
RudRecciah <me@rudrecciah.dev>
2021-06-01 18:56:46 -0400

    a bit of refactoring

--- js/main.js
+++ js/main.js
@@ -8,7 +8,7 @@ async function make_news() {
       await new Promise(r => setTimeout(r, 100));
       let color = rgb_shade(-0.1, post.color);
       const article = document.createElement("div");
-      article.setAttribute("class", `card text-center animate__animated animate__bounceIn`);
+      article.setAttribute("class", "card text-center animate__animated animate__bounceIn");
       article.setAttribute("style", `background-color: ${color};`);
       const title = document.createElement("p");
       title.setAttribute("class", "card-header");
@@ -18,10 +18,10 @@ async function make_news() {
       author.setAttribute("class", "author-text");
       author.innerText = post.author;
       const img = document.createElement("img");
-      img.setAttribute("src", "data:image/png;base64," + post.image.base64);
-      img.setAttribute("alt", post.author + "'s post image");
+      img.setAttribute("src", `data:image/png;base64,${post.image.base64}`);
+      img.setAttribute("alt", `${post.author}'s post image`);
       img.setAttribute("class", "card-img-top m-auto pt-2 pb-2");
-      img.setAttribute("style", `max-height: 500px; max-width: 85%;`);
+      img.setAttribute("style", "max-height: 500px; max-width: 85%;");
       title.appendChild(titleContent);
       title.appendChild(author);
 
@@ -95,49 +95,49 @@ async function load()
 
   agree_button.setAttribute("type", "button");
   agree_button.setAttribute("href", "https://discord.com/api/oauth2/authorize?client_id=822120932944117770&redirect_uri=http%3A%2F%2F127.0.0.1%3A5500%2F&response_type=code&scope=identify");
-  agree_button.innerText = 'Continue';
+  agree_button.innerText = "Continue";
 
   var agree_section = document.getElementById("agree_section");
 
-  checkbox.addEventListener('change', function() {
-    if (this.checked) {
+  checkbox.addEventListener("change", function() {
+    if(this.checked) {
       agree_section.appendChild(agree_button);
-    } else {
+    }else{
       agree_section.removeChild(agree_button);
     }
   });
 
   const fragment = new URLSearchParams(window.location.search.slice(1));
-  const code = fragment.get('code');
+  const code = fragment.get("code");
     
-    if (code) 
-    { 
-      fetch('https://s1.thomasricci.dev:2087/login/', {
-        method: 'POST',
-        mode: 'cors',
-        cache: 'no-cache',
-        credentials: 'same-origin',
-        headers: {
-					'Content-Type': 'text/plain',
-				},
-        body: JSON.stringify({"code": `${code}`,})
-      })
+  if(code) 
+  { 
+    fetch("https://s1.thomasricci.dev:2087/login/", {
+      method: "POST",
+      mode: "cors",
+      cache: "no-cache",
+      credentials: "same-origin",
+      headers: {
+        "Content-Type": "text/plain",
+      },
+      body: JSON.stringify({"code": `${code}`, })
+    })
       .then(result => result.json())
       .then(response => 
       {
         console.log(response);
         if(!("error" in response))
         {
-          const{ id, avatar, session } = response;
+          const { id, avatar, session } = response;
           const { username, discriminator } = response;
   
-          var usernameField = document.getElementById('profile_button');
+          var usernameField = document.getElementById("profile_button");
           usernameField.setAttribute("data-toggle", "dropdown");
           usernameField.removeAttribute("data-target");
 
           let joindate_display = document.getElementById("joindate_display");
 
-          joindate_display.innerText = "Joined on " + response.join;
+          joindate_display.innerText = `Joined on ${response.join}`;
 
           let pictureLink = "https://cdn.discordapp.com/avatars/" + `${id}` + "/" +  `${avatar}` + ".png";
           let usernameInfo = document.createTextNode(` ${username}#${discriminator}`);
@@ -156,54 +156,54 @@ async function load()
           createPostTab.classList.remove("disabled");
           shopTab.classList.remove("disabled");
 
-          usernameField.setAttribute('href', 'http://127.0.0.1:5500/');
+          usernameField.setAttribute("href", "http://127.0.0.1:5500/");
 
-          fetch('https://s1.thomasricci.dev:2087/coin/get/', {
-            method: 'POST',
-            mode: 'cors',
-            cache: 'no-cache',
-            credentials: 'same-origin',
+          fetch("https://s1.thomasricci.dev:2087/coin/get/", {
+            method: "POST",
+            mode: "cors",
+            cache: "no-cache",
+            credentials: "same-origin",
             headers: {
-              'Content-Type': 'text/plain',
+              "Content-Type": "text/plain",
             },
-            body: JSON.stringify({"id": `${id}`,})
+            body: JSON.stringify({"id": `${id}`, })
           })
-          .then(result => result.json())
-          .then(response =>
-          {
-            document.getElementById("coin-count").innerHTML = `${response.balance} Rax Coins`;
-          })
+            .then(result => result.json())
+            .then(response =>
+            {
+              document.getElementById("coin-count").innerHTML = `${response.balance} Rax Coins`;
+            });
 
-          fetch('https://s1.thomasricci.dev:2087/coin/set/', {
-            method: 'POST',
-            mode: 'cors',
-            cache: 'no-cache',
-            credentials: 'same-origin',
+          fetch("https://s1.thomasricci.dev:2087/coin/set/", {
+            method: "POST",
+            mode: "cors",
+            cache: "no-cache",
+            credentials: "same-origin",
             headers: {
-              'Content-Type': 'text/plain',
+              "Content-Type": "text/plain",
             },
             body: JSON.stringify({"id": `${id}`, "session": `${session}`, "positive": true, "change": 1})
           })
-          .then(result => result.json())
-          .then(response =>
-          {
-            console.log(response);
-            if(response.success)
+            .then(result => result.json())
+            .then(response =>
             {
-              document.getElementById("coin-count").innerHTML = `${response.newBal} Rax Coins`; 
-            }
-          })
+              console.log(response);
+              if(response.success)
+              {
+                document.getElementById("coin-count").innerHTML = `${response.newBal} Rax Coins`; 
+              }
+            });
 
           //To not get authentication error on reload because of the parameter saved in the URL
           window.history.replaceState({}, document.title, "/");
           
         }
       }).catch(console.error);
-  };
+  }
 
   var disclaimer = document.getElementById("disclaimer");
 
-  if(localStorage.getItem("agreed_with") == "1.0")
+  if(localStorage.getItem("agreed_with") === "1.0")
   {
     disclaimer.style.setProperty("display", "none");
   }
@@ -213,13 +213,13 @@ async function load()
   await make_news();
 }
 
-const rgb_shade=(p,c)=>
+const rgb_shade=(p, c) =>
 {
-    var i=parseInt,r=Math.round,[a,b,c,d]=c.split(","),P=p<0,t=P?0:255*p,P=P?1+p:1-p;
-    return"rgba"+(d?"a(":"(")+r(i(a[3]=="a"?a.slice(5):a.slice(4))*P+t)+","+r(i(b)*P+t)+","+r(i(c)*P+t)+(d?","+d:", 0.7)");
-}
+  var i=parseInt, r=Math.round, [a, b, c, d]=c.split(","), P=p<0, t=P?0:255*p, P=P?1+p:1-p;
+  return `rgba${d?"a(":"("}${r(i(a[3]=="a"?a.slice(5):a.slice(4))*P+t)},${r(i(b)*P+t)},${r(i(c)*P+t)}${d?`,${d}`:", 0.7)"}`;
+};
 
-const get_profile_picture=(user_id, image_id)=>
+const get_profile_picture=(user_id, image_id) =>
 {
   return "https://cdn.discordapp.com/avatars/" + `${user_id}` + "/" +  `${image_id}` + ".png";
-}
+};
